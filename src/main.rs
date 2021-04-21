6858d744a844d86c61fb40e922c0754ba66f7eb2
Jochen Schieberlein <jochen.schieberlein@thinprint.com>
2021-04-21 14:18:23 +0200

    optimize code

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::io::{BufRead, BufReader};
 use regex::Regex;
 use std::fs::File;
+// https://doc.rust-lang.org/std/collections/struct.HashMap.html
 // use std::collections::HashMap;
 use std::fs::OpenOptions;
 use std::io::Write;
@@ -18,26 +19,18 @@ fn main() {
 
     let re = Regex::new(splitregex).unwrap();
 
-    // Read the file line by line using the lines() iterator from std::io::BufRead.
     for (_index, line) in reader.lines().enumerate() {
         let line = line.unwrap(); // Ignore errors.
-        if re.is_match(&line)
-        {
-            for cap in re.captures_iter(&line) {
-                let rawfilename = format!("{}.log", &cap[1]);
-                let dfilename = &rawfilename.replace("/", "_").replace(":", "_").replace("?", "_").replace("*", "_").replace("\\", "_").replace("\"", "_");
-                println!("{}.{} open file", _index + 1, dfilename);
+        for cap in re.captures_iter(&line) {
+            let rawfilename = format!("{}.log", &cap[1]);
+            let dfilename = &rawfilename.replace("/", "_").replace(":", "_").replace("?", "_").replace("*", "_").replace("\\", "_").replace("\"", "_");
+            println!("{}.{} open file", _index + 1, dfilename);
 
-                let fileexsits = std::path::Path::new(dfilename).exists();
-                if fileexsits == false {
-                    let  _file_out = std::fs::File::create(dfilename).unwrap();
-                }
-                let mut file = OpenOptions::new().append(true).open(dfilename).expect(
-                    "cannot open file");
-                file.write_all(line.as_bytes()).expect("write failed");
-                file.write_all("\n".as_bytes()).expect("write failed");
-                // println!("{}.{}: {}", index + 1, dfilename, line);
-            }
+            let mut file = OpenOptions::new().append(true).create(true).open(dfilename).expect(
+                "cannot open file");
+            file.write_all(line.as_bytes()).expect("write failed");
+            file.write_all("\n".as_bytes()).expect("write failed");
+            // println!("{}.{}: {}", index + 1, dfilename, line);
         }
     }
 }
