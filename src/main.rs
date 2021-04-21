1afd118db485ebe42bde0d0e8722e9f9eb3d5b7c
Jochen Schieberlein <jochen.schieberlein@thinprint.com>
2021-04-21 15:26:56 +0200

    fist fast version

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::env;
-use std::io::{BufRead, BufReader};
 use regex::Regex;
+use std::env;
 use std::fs::File;
+use std::io::{BufRead, BufReader};
 // https://doc.rust-lang.org/std/collections/struct.HashMap.html
-// use std::collections::HashMap;
+use std::collections::HashMap;
 use std::fs::OpenOptions;
 use std::io::Write;
 
@@ -16,18 +16,34 @@ fn main() {
     // Open the file in read-only mode (ignoring errors).
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
-
     let re = Regex::new(splitregex).unwrap();
+    let mut open_files: HashMap<String, File> = HashMap::new();
 
-    for (_index, line) in reader.lines().enumerate() {
-        let line = line.unwrap(); // Ignore errors.
+    for (index, line) in reader.lines().enumerate() {
+        //  iconv -s -f "CP1252" -t UTF-8 "thinmon.log" > ttt
+        if line.is_err() {
+            println!("Error in line: {}", index);
+        }
+        let line = line.unwrap();
         for cap in re.captures_iter(&line) {
             let rawfilename = format!("{}.log", &cap[1]);
-            let dfilename = &rawfilename.replace("/", "_").replace(":", "_").replace("?", "_").replace("*", "_").replace("\\", "_").replace("\"", "_");
-            println!("{}.{} open file", _index + 1, dfilename);
+            let dfilename = &rawfilename
+                .replace("/", "_")
+                .replace(":", "_")
+                .replace("?", "_")
+                .replace("*", "_")
+                .replace("\\", "_")
+                .replace("\"", "_");
+            // println!("{}.{} open file", _index + 1, dfilename);
 
-            let mut file = OpenOptions::new().append(true).create(true).open(dfilename).expect(
-                "cannot open file");
+            let file = open_files.entry(dfilename.to_string()).or_insert(
+                OpenOptions::new()
+                    .append(true)
+                    .create(true)
+                    .open(dfilename)
+                    .expect("cannot open file"),
+            );
+
             file.write_all(line.as_bytes()).expect("write failed");
             file.write_all("\n".as_bytes()).expect("write failed");
             // println!("{}.{}: {}", index + 1, dfilename, line);
