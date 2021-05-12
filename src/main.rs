3ff7340aef3a2f2c7415f6d403681505b8480c2e
Jochen Schieberlein <jochen.schieberlein@thinprint.com>
2021-05-12 14:18:35 +0200

    also store not maching lines in the split files

--- src/main.rs
+++ src/main.rs
@@ -54,8 +54,34 @@ struct Opts {
     command: Command,
 }
 
+fn write_line_to_key_file(findings: &mut HashMap<String, File>, key: &str, line: &str) {
+
+    let file = findings.entry(key.to_string()).or_insert(
+        OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open(
+                &key.replace(">", "_")
+                    .replace("<", "_")
+                    .replace(":", "_")
+                    .replace("?", "_")
+                    .replace("*", "_")
+                    .replace(r"/", "_")
+                    .replace(r"\", "_")
+                    .replace(r"|", "_")
+                    .replace("\"", "_"),
+            )
+            .expect("cannot open file"),
+    );
+
+    file.write_all(line.as_bytes())
+        .and(file.write_all(b"\n"))
+        .expect("write failed");
+}
+
 fn split(reader: BufReader<File>, re: Regex) {
     let mut findings: HashMap<String, File> = HashMap::new();
+    let mut last_used_file_name: String = "".to_string();
 
     for (index, line) in reader.lines().enumerate() {
         //  iconv -s -f "CP1252" -t UTF-8 "thinmon.log" > ttt
@@ -63,39 +89,23 @@ fn split(reader: BufReader<File>, re: Regex) {
             println!("Error in line: {}", index);
         }
         let line = line.unwrap();
-        for cap in re.captures_iter(&line) {
-            let rawfilename = format!("{}.log", &cap[1]);
-            // println!("{}.{} open file", _index + 1, dfilename);
-
-            let file = findings.entry(rawfilename.to_string()).or_insert(
-                OpenOptions::new()
-                    .append(true)
-                    .create(true)
-                    .open(
-                        &rawfilename
-                            .replace(">", "_")
-                            .replace("<", "_")
-                            .replace(":", "_")
-                            .replace("?", "_")
-                            .replace("*", "_")
-                            .replace(r"/", "_")
-                            .replace(r"\", "_")
-                            .replace(r"|", "_")
-                            .replace("\"", "_"),
-                    )
-                    .expect("cannot open file"),
-            );
-
-            file.write_all(line.as_bytes())
-                .and(file.write_all(b"\n"))
-                .expect("write failed");
-            // println!("{}.{}: {}", index + 1, dfilename, line);
+        if let Some(caps) = re.captures(&line) {
+            let cap = caps.get(1).map_or("", |m| m.as_str());
+            let rawfilename = format!("{}.log", cap);
+            last_used_file_name = rawfilename.to_string();
+            write_line_to_key_file(&mut findings, &rawfilename, &line);
+        } else {
+            if last_used_file_name.is_empty() {
+                continue;
+            }
+            write_line_to_key_file(&mut findings, &last_used_file_name, &line);
         }
     }
 }
 
 fn count(reader: BufReader<File>, re: Regex, args: Vec<String>) {
-    let mut findings: HashMap<String, u32> = HashMap::new();
+    let mut count_findings: HashMap<String, u32> = HashMap::new();
+    let mut found_all = 0;
 
     for (index, line) in reader.lines().enumerate() {
         //  iconv -s -f "CP1252" -t UTF-8 "thinmon.log" > ttt
@@ -104,20 +114,21 @@ fn count(reader: BufReader<File>, re: Regex, args: Vec<String>) {
         }
         let line = line.unwrap();
         for cap in re.captures_iter(&line) {
-            findings
+            found_all = found_all + 1;
+            count_findings
                 .entry(cap[1].to_string())
                 .and_modify(|c| *c = *c + 1)
                 .or_insert(1);
         }
     }
 
-    let mut sorted: Vec<(_, _)> = findings.iter().collect();
+    let mut sorted: Vec<(_, _)> = count_findings.iter().collect();
     sorted.sort_by(|a, b| a.1.cmp(b.1).reverse());
 
-    if findings.len() > 0 {
+    if count_findings.len() > 0 {
         println!("{:?}", &args[1..]);
-        println!("found {} unique results", findings.len());
-        // for (name, count) in findings {
+        println!("found {} overall results", found_all);
+        println!("found {} unique results", count_findings.len());
         for (name, count) in sorted {
             println!(
                 "{cnt:>5} {n}",
